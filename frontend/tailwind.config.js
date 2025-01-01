e1276f073a6796e6c0cb412ba95ca481b5c44c49
meet <dodiyameet041@gmail.com>
2025-01-01 15:37:08 +0530

    Deployment failed for require is not define fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import * as tailwindAnimate from "tailwindcss-animate"
 export default {
     darkMode: ["class"],
     content: [
@@ -88,5 +89,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 }
